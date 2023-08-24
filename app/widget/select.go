853601173ae60d47ef208419a547790c08c2da15
tinymeng <666@majiameng.com>
2023-08-24 23:43:42 +0800

    Changes

--- app/widget/select.go
+++ app/widget/select.go
@@ -20,39 +20,35 @@ type SelectParams struct {
 }
 
 /**
-gender|0|性别|name|id
-1=男,2=女,3=保密
-0
+from: gender|0|性别|name|id
+option: 1=男,2=女,3=保密
+value: 0
 */
 func Select(from string, option string, value interface{}) template.HTML {
 	fmt.Println("11============")
-	fmt.Println(from) //gender|0|性别|name|id
-	fromData := strings.Split(from, "|")
-	fmt.Println(fromData) //[gender 0 性别 name id]
-
-	//var selectParams SelectParams
-	//for {
-	//
-	//}
-
-	//fmt.Println(22222, selectPar)
-
-	//for _, v := range list {
-	//	adSortList[v.Id] = v.Description
-	//}
-
-	fmt.Println(option)
-	fmt.Println(value)
+	fromArray := strings.Split(from, "|")
+	optionArray := strings.Split(option, ",")
+	//表单名称 是否必传 提示问题 name值 id值
+	fromName, must, placeholder, name, id := fromArray[0], fromArray[1], fromArray[2], fromArray[3], fromArray[4]
+	fmt.Println(name, id)
 
 	html := ""
 	html += `
-    <select>
-      <option value="">请选择</option>
-      <option value="AAA">选项 A</option>
-      <option value="BBB">选项 B</option>
-      <option value="CCC">选项 C</option>
-    </select>
-`
+    <select name="` + fromName + `" `
+	if must == "1" {
+		html += `lay-verify="required"`
+	}
+	if placeholder != "" {
+		html += `placeholder="` + placeholder + `"`
+	}
+	html += `><option value="">请选择</option>`
+	for _, option := range optionArray {
+		optionArray := strings.Split(option, "=")
+		html += `<option value="` + optionArray[0] + `">` + optionArray[1] + `</option>`
+		fmt.Println(optionArray)
+	}
+	html += `</select>`
+	fmt.Println(html)
 	rt := template.HTML(html)
 	return rt
 }
