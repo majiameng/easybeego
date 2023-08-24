6a20f21643059b0660f3a3cd8450e17427080f32
tinymeng <666@majiameng.com>
2023-08-24 23:02:08 +0800

    Changes

--- app/widget/select.go
+++ app/widget/select.go
@@ -3,26 +3,56 @@ package widget
 import (
 	"fmt"
 	"html/template"
+	"strings"
 )
 
+//type SelectParams struct {
+//	from   string
+//	option string
+//	value  interface{}
+//}
+
 type SelectParams struct {
-	from   string
-	option string
-	value  interface{}
+	one   string
+	two   string
+	three string
+	four  string
 }
 
+/**
+gender|0|性别|name|id
+1=男,2=女,3=保密
+0
+*/
 func Select(from string, option string, value interface{}) template.HTML {
 	fmt.Println("11============")
-	fmt.Println(from)
+	fmt.Println(from) //gender|0|性别|name|id
+	fromData := strings.Split(from, "|")
+	fmt.Println(fromData) //[gender 0 性别 name id]
+
+	//var selectParams SelectParams
+	//for {
+	//
+	//}
+
+	//fmt.Println(22222, selectPar)
+
+	//for _, v := range list {
+	//	adSortList[v.Id] = v.Description
+	//}
+
 	fmt.Println(option)
 	fmt.Println(value)
-	rt := template.HTML(`
+
+	html := ""
+	html += `
     <select>
       <option value="">请选择</option>
       <option value="AAA">选项 A</option>
       <option value="BBB">选项 B</option>
       <option value="CCC">选项 C</option>
     </select>
-`)
+`
+	rt := template.HTML(html)
 	return rt
 }
